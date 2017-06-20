1b0c2352902754f5df0516b01b12166e822c40d4
demmm <anke62@gmail.com>
2017-06-20 12:58:05 -0400

    merge transifex fr

--- lang/welcome_fr.ts
+++ lang/welcome_fr.ts
@@ -4,32 +4,32 @@
     <message>
         <location filename="../Page1Form.ui.qml" line="41"/>
         <source>Install KaOS</source>
-        <translation type="unfinished"/>
+        <translation>Installer KaOS</translation>
     </message>
     <message>
         <location filename="../Page1Form.ui.qml" line="64"/>
         <source>About</source>
-        <translation type="unfinished"/>
+        <translation>À propos</translation>
     </message>
     <message>
         <location filename="../Page1Form.ui.qml" line="87"/>
         <source>Guide</source>
-        <translation type="unfinished"/>
+        <translation>Guide</translation>
     </message>
     <message>
         <location filename="../Page1Form.ui.qml" line="128"/>
         <source>Package List</source>
-        <translation type="unfinished"/>
+        <translation>Liste des paquets</translation>
     </message>
     <message>
         <location filename="../Page1Form.ui.qml" line="152"/>
         <source>Forum</source>
-        <translation type="unfinished"/>
+        <translation>Forum</translation>
     </message>
     <message>
         <location filename="../Page1Form.ui.qml" line="175"/>
         <source>Documentation</source>
-        <translation type="unfinished"/>
+        <translation>Documentation</translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,7 @@
     <message>
         <location filename="../main.qml" line="32"/>
         <source>Welcome</source>
-        <translation type="unfinished"/>
+        <translation>Bienvenue</translation>
     </message>
     <message>
         <location filename="../main.qml" line="41"/>
@@ -45,7 +45,10 @@
 Want to learn more about the project? Please find the links below.
 
 Don&apos;t hesitate to give your opinion about KaOS in the Forum.</source>
-        <translation type="unfinished"/>
+        <translation>Merci d’essayer KaOS. Vous recherchez un fichier de mots de passe ou d’une liste de paquets ?
+Vous voulez en savoir plus sur le projet ? Veuillez cliquer sur les liens ci-dessous.
+
+N’hésitez pas à partager votre avis sur KaOS dans le forum.</translation>
     </message>
     <message>
         <location filename="../main.qml" line="53"/>
@@ -57,17 +60,23 @@ live               live
 
 Hope you enjoy</source>
         <extracomment>Don not translate root/root and live/live</extracomment>
-        <translation type="unfinished"/>
+        <translation>Paramètres de connexion de la session live :
+
+Utilisateur:   Mot de passe:
+root               root
+live               live
+
+Amusez-vous bien !</translation>
     </message>
     <message>
         <location filename="../main.qml" line="78"/>
         <source>Main</source>
-        <translation type="unfinished"/>
+        <translation>Principal</translation>
     </message>
     <message>
         <location filename="../main.qml" line="87"/>
         <source>Passwords</source>
-        <translation type="unfinished"/>
+        <translation>Mots de passe</translation>
     </message>
 </context>
 </TS>
