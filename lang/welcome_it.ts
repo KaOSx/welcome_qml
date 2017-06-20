89dd3f5d53fc426411bb4399a30cabc885928ef7
demmm <anke62@gmail.com>
2017-06-20 14:46:29 -0400

    add translator code to main.cpp updated .ts & .qm files

--- lang/welcome_it.ts
+++ lang/welcome_it.ts
@@ -1,35 +1,37 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="it" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
 <context>
     <name>Page1Form.ui</name>
     <message>
         <location filename="../Page1Form.ui.qml" line="41"/>
         <source>Install KaOS</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Page1Form.ui.qml" line="64"/>
         <source>About</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Page1Form.ui.qml" line="87"/>
         <source>Guide</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Page1Form.ui.qml" line="128"/>
         <source>Package List</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Page1Form.ui.qml" line="152"/>
         <source>Forum</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../Page1Form.ui.qml" line="175"/>
         <source>Documentation</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -37,7 +39,7 @@
     <message>
         <location filename="../main.qml" line="32"/>
         <source>Welcome</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main.qml" line="41"/>
@@ -45,7 +47,7 @@
 Want to learn more about the project? Please find the links below.
 
 Don&apos;t hesitate to give your opinion about KaOS in the Forum.</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main.qml" line="53"/>
@@ -57,17 +59,17 @@ live               live
 
 Hope you enjoy</source>
         <extracomment>Don not translate root/root and live/live</extracomment>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main.qml" line="78"/>
         <source>Main</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main.qml" line="87"/>
         <source>Passwords</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <QTranslator>
+#include <QLocale>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
+
+    QString locale = QLocale::system().name();
+    QTranslator translator;
+
+    translator.load(locale, ":/lang");
+    app.installTranslator(&translator);
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
